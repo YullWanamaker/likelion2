5978d1d874e822d7a32191085a860721795294ad
Wanamaker <dla4038@naver.com>
2022-08-24 18:59:39 +0900

    good

--- App.js
+++ App.js
@@ -1,21 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+  Button,
+  Alert,
+  Scrollview,
+} from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View
+      style={{
+        height: 200,
+        padding: 50,
+      }}
+    >
+      <Text
+        style={{
+          fontSize: 30,
+          fontWeight: "900",
+        }}
+      >
+        Hi yulls
+      </Text>
+      <Image
+        style={{
+          width: 400,
+          heihgt: 40,
+        }}
+        source={require("./image/doo.png")}
+      />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
